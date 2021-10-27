0f2f8ff41c96009c49dbdad5dacac289905548e3
David Viana <davidviana1706@gmail.com>
2021-10-27 10:19:32 -0300

    forth commit

---
+++ header-mobile/main.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFuntional() {
+    var x = document.getElementById("myLink");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
