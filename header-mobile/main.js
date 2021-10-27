c5eaa1916737f80e6df12d2f5bf4760bf6689417
David Viana <davidviana1706@gmail.com>
2021-10-27 15:55:05 -0300

    fifith commit

--- header-mobile/main.js
+++ header-mobile/main.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function abrirCards() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
@@ -7,8 +7,45 @@ function myFunction() {
     }
 }
 
-function myFuntional() {
-    var x = document.getElementById("myLink");
+
+function abrirCardsFi() {
+    var x = document.getElementById("card-first");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsSe() {
+    var x = document.getElementById("card-second");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsTh() {
+    var x = document.getElementById("card-third");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsFo() {
+    var x = document.getElementById("card-fourth");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsFiv() {
+    var x = document.getElementById("card-five");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
