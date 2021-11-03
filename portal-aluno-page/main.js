15c3b3651c650868c766915bd0608b3c2a1ef0e6
David Viana <davidviana1706@gmail.com>
2021-11-03 15:46:47 -0300

    update commit

--- portal-aluno-page/main.js
+++ portal-aluno-page/main.js
@@ -7,16 +7,43 @@ function mostrarSenha() {
     }
 }
 
-function Valida() {
-    var id = "professor"
+function validaUser() {
+    let useradm = "admgerminare"
+    var id = document.getElementById("user")
 
-    var user = document.getElementById("user")
-    var users = user.value
+    if(useradm === id.value) {
+        user.classList.add("correct")
+        user.classList.remove("incorrect")
 
-    if(users === id ){
-        SubmitEvent
-        alert("DEI SUBMIT")
+        var msg = document.getElementById("user-msg")
+        msg.style.display = "none"
     } else {
-        user.innerHTML = `Digite a senha novamente`
+        user.classList.remove("correct")
+        user.classList.add("incorrect")
+    }
+}
+
+function validaPassword() {
+    let senhaadm = "odetedict"
+    let senha = document.getElementById("password")
+
+    if(senhaadm === senha.value) {
+        password.classList.remove("incorrect")
+        password.classList.add("correct")
+
+        var msg = document.getElementById("password-msg")
+        msg.style.display = "none"
+    } else {
+        password.classList.remove("correct")
+        password.classList.add("incorrect")
+    }
+}
+
+function enterPage() {
+    if(password.classList.contains("correct") && user.classList.contains("correct")){
+        var link = document.getElementById("jezus")
+        link.href = "./workspace-page.html"
+    } else {
+        return
     }
 }
