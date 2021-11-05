b85c88aba7e4263907934ab4e0132824ced6bf6c
David Viana <davidviana1706@gmail.com>
2021-11-05 17:25:05 -0300

    update commit

---
+++ contact-page/main.js
@@ -0,0 +1,61 @@
+function abrirCards() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsFi() {
+    var x = document.getElementById("card-first");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsSe() {
+    var x = document.getElementById("card-second");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsTh() {
+    var x = document.getElementById("card-third");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsFo() {
+    var x = document.getElementById("card-fourth");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function abrirCardsFiv() {
+    var x = document.getElementById("card-five");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.navbar').css("background-url", "dist/img/logo1.png")
+    } else {
+        $('.navbar').css("background-url", "dist/img/logo2.png")
+    }
+});
